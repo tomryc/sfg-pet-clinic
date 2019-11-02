d24aecf1a097fa07b5391e86fc797ca1c165e82e
Tomasz Rycerz <tomryc@gmail.com>
2019-11-02 17:32:33 +0000

    Closes #67.

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/VisitControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/VisitControllerTest.java
@@ -27,7 +27,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
+//Closes #67
 @ExtendWith(MockitoExtension.class)
 class VisitControllerTest {
 
